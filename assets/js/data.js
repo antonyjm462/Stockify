d2a03e8ffb0050563eb9de0986c4709957494611
Antony.j.m <antonyjosemalakkaran@gmail.com>
2019-08-14 20:52:37 +0630

    fav

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,22 +3,22 @@ var make_chart = function(json) {
     var data = [];
     for (var key in json) {
         if (json.hasOwnProperty(key)) {
-            console.log(json.Symbol, json.Open);
-            symbol.push(json.Symbol);
-            data.push(json.Open);
+            console.log(json[key]);
+            symbol.push(json[key].Symbol)
+            data.push(json[key].Open)
         }
     }
 
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
         // The type of chart we want to create
-        type: 'line',
+        type: 'bar',
 
         // The data for our dataset
         data: {
             labels: symbol,
             datasets: [{
-                label: 'My First dataset',
+                label: "Stocks Today",
                 backgroundColor: 'rgb(255, 99, 132)',
                 borderColor: 'rgb(255, 99, 132)',
                 data: data,
